dc393eb527ea5788ad2afe71cc3a00c30336c459
Loup <loumarti@student.42lyon.fr>
2024-03-25 15:34:29 +0100

    dapp wip, broken but need to stick with ethers lib doc tu understand out this

--- dappFive/frontend/five_react/src/App.js
+++ dappFive/frontend/five_react/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState, useEffect, Component } from 'react';
-import Web3 from 'web3'
+import { ethers } from "ethers";
+
 
 
 // class App extends Component {
@@ -32,6 +33,8 @@ import Web3 from 'web3'
 // export default App;
 
 
+//------------------------------------------------------------------
+
 function App() {
 
   const [fiveToken, setFiveToken] = useState(0);
@@ -41,71 +44,61 @@ function App() {
   const [accountBalance, setAccountBalance] = useState(0);
   const [accountStakes, setAccountStakes] = useState({});
 
-
   useEffect(() => {
     // Here we check if there is web3 support / if metamask is installed
-    if (typeof window.ethereum !== 'undefined') {
+    connectWallet();
+    getWalletInfos();
 
-      // console.log("window.ethereum :", window.ethereum);
-      
-      const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-      
-
-      const fetchAccounts = async () => {
-        const accounts = await web3.eth.getAccounts();
-        setAccounts(accounts);
-      }
-
-      const getFiveTokenContract = async () => {
-        const abi = await getABI();
-        const address = getFiveTokenContractAddress();
-        const fiveToken = new web3.eth.Contract(abi, address); // Make a new instance of the contract'
-        setFiveToken(fiveToken);
-      }
-
-      fetchAccounts();
-      getFiveTokenContract();
-
-
-
-
-      //the MetaMask window.web3.currentProvider shim. This property is deprecated
-      // if (window.web3.currentProvider.isMetaMask === true) {
-      //     // connectMetaMask();
-      //     // connectToSelectedNetwork(web3);
-      //     console.log("window.web3.currentProvider.isMetaMask === true");
-      //   } else {
-      //     // Another web3 provider, add support if you want
-      //     throw new Error("Only metamask supported yet");
-      //   }
-
-      // Request user's account address
-      // window.ethereum.request({ method: 'eth_requestAccounts' })
-      // .then((accounts) => {
-      //   console.log("User's accounts[0] address:", accounts[0]);
-      //   setAccounts(accounts);
-      //   // connectToSelectedNetwork(web3);
-      // })
-      // .catch((error) => {
-      //   throw new Error(error);
-      // });
-
-    } else {
-      // MetaMask is not installed
-      throw new Error("MetaMask is not installed. Please install it to use this DAPP.");
-    }
+    
   }, []);
 
-  useEffect(() => {
-    if (loaded && (accounts !== 0) && (fiveToken !== 0)) {
-      // console.log("accounts :", accounts);
-      getUserProfile();
-    } else {
-    // dirty trick to trigger reload if something went wrong
-      setTimeout(setLoaded(true), 500);
-    }
-  }, [loaded, accounts, fiveToken]);
+  async function connectWallet() {
+    try {
+      await window.ethereum.enable();
+      if (window.ethereum) {
+        // Request account access
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
 
+        setAccounts(accounts);
+      } else {
+        console.error('MetaMask is needed to use this dapp.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getWalletInfos() {
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = provider.getSigner();
+
+        const abi = await getABI();
+        const address = getFiveTokenContractAddress();
+
+        const contract = new ethers.Contract(address, abi, provider);
+        setFiveToken(contract);
+        
+        const totalSupply = await contract.totalSupply();
+        console.log("totalSupply = ", totalSupply);
+        setTotalSupply(ethers.formatEther(totalSupply));
+
+        const accountBalance = await contract.balanceOf(accounts[0]);
+        console.log("accountBalance = ", accountBalance);
+        setAccountBalance(accountBalance);
+
+
+      } else {
+        throw new Error("Can't getWalletInfos from Metamask.");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    
+  }
 
   async function getABI() {
     let ABI = "";
@@ -127,74 +120,177 @@ function App() {
       throw new Error(error);
     });
 
-    return ABI;
-  }
-
-  async function getUserProfile() {
-  
-    call(fiveToken.methods.balanceOf, setAccountBalance, accounts[0]);
-    call(fiveToken.methods.hasStake, setAccountStakes, accounts[0]);
-    call(fiveToken.methods.totalSupply, setTotalSupply);
-
-    console.log("totalSupply = ", totalSupply);
-    // await fiveToken.methods.totalSupply().call()
-    // .then((result) => {
-    //   setTotalSupply(result);
-    // })
-    // .catch((error) => {
-    //   throw new Error(error);
-    // })
+    if (Array.isArray(ABI) && ABI.length > 0) {
+        return ABI;
+      } else {
+        throw new Error('Invalid ABI format.');
+      }
   }
 
   // A hardcoded shortcut with FiveToken contract address from ganache 
+  // GET CORRECT CONTRACT ADDRESS IN GANACHE
   function getFiveTokenContractAddress() {
-    return "0x1917b8513697Cf919eec8E848b139013c14C8402";
+    // return "0x1917b8513697Cf919eec8E848b139013c14C8402";
+    return "0x30Dbdcb045f6EB1A058c10f2acdFC69C26e3F3c0";
   }
 
-  function call(func, callback, ...args) {
-    func(...args).call()
-    .then((result) => {
-      callback(result);
-    })
-    .catch((error) => {
-      throw new Error(error);
-    })
-  }
 
-  // async function connectToSelectedNetwork(web3) {
-  //   const abi = await getABI();
-  //   const address = getFiveTokenContractAddress();
 
-  //   // [!] web3.eth UNDEFINED !
-  //   const fiveToken = new web3.eth.Contract(abi, address); // Make a new instance of the contract'
-  //   setFiveToken(fiveToken);
-  // }
 
-   
-  // function connectMetaMask() {
-  //     // We need to make the connection to MetaMask work.
-  //     // Send Request for accounts and to connect to metamask.
-  //     window.web3.requestAccounts()
-  //     .then((result) => {
-  //     // Whenever the user accepts this will trigger
-  //     console.log(result)
-  //     })
-  //     .catch((error) => {
-  //     // Handle errors, such as when a user does not accept
-  //     throw new Error(error);
-  //     });
-  // };
 
   return (
-    <div className="App">
-        <header className="App-header">
-            <h1> Welcome to your DAPP application</h1>
-            <p> Account : {accounts[0]}</p>
-            <p>FiveToken total supply : {totalSupply}</p>
-            <button ><p>Stake</p></button>
-        </header>
-    </div>
-  );
+        <div className="App">
+            <header className="App-header">
+                <h1> Welcome to your DAPP application</h1>
+                <p> Account : {accounts[0]}</p>
+                {totalSupply === 0 && <p>FiveToken total supply: 0</p>}
+                {totalSupply !== 0 && <p>FiveToken total supply: {totalSupply}</p>}
+                <p> Your FiveToken balance: {accountBalance}</p>
+                <button ><p>Stake</p></button>
+            </header>
+        </div>
+      );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------
+// function App() {
+
+//   const [fiveToken, setFiveToken] = useState(0);
+//   const [accounts, setAccounts] = useState(0);
+//   const [loaded, setLoaded] = useState(false);
+//   const [totalSupply, setTotalSupply] = useState(0);
+//   const [accountBalance, setAccountBalance] = useState(0);
+//   const [accountStakes, setAccountStakes] = useState({});
+
+
+//   useEffect(() => {
+//     // Here we check if there is web3 support / if metamask is installed
+//     if (typeof window.ethereum !== 'undefined') {      
+//       const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+      
+
+//       const fetchAccounts = async () => {
+//         const accounts = await web3.eth.getAccounts();
+//         setAccounts(accounts);
+//       }
+
+//       const getFiveTokenContract = async () => {
+//         const abi = await getABI();
+//         const address = getFiveTokenContractAddress();
+//         const fiveToken = new web3.eth.Contract(abi, address); // Make a new instance of the contract'
+//         setFiveToken(fiveToken);
+//       }
+
+//       fetchAccounts();
+//       getFiveTokenContract();
+
+//     } else {
+//       // MetaMask is not installed
+//       throw new Error("MetaMask is not installed. Please install it to use this DAPP.");
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     if (loaded && (accounts !== 0) && ((fiveToken !== 0) || (typeof fiveToken.methods !== 'undefined'))) {
+//       // console.log("accounts :", accounts);
+//       getUserProfile();
+//     } else {
+//     // dirty trick to trigger reload if something went wrong
+//       setTimeout(setLoaded(true), 500);
+//     }
+//   }, [loaded, accounts]);
+
+
+//   async function getABI() {
+//     let ABI = "";
+
+//     await fetch("./FiveToken.json", {
+//       headers : {
+//         'Accept': 'application.json',
+//         'Content-Type': 'application.json',
+//       }
+//     }).then((response) => {
+//       if (response.status === 200) {
+//         return response.json();
+//       } else {
+//         throw new Error('Error fetching ABI (FiveToken.json)');
+//       }
+//     }).then((data) => {
+//       ABI = data.abi; // [!] need abi, not all json
+//     }).catch((error) => {
+//       throw new Error(error);
+//     });
+
+//     if (Array.isArray(ABI) && ABI.length > 0) {
+//         return ABI;
+//       } else {
+//         throw new Error('Invalid ABI format.');
+//       }
+//   }
+
+//   async function getUserProfile() {
+  
+//     call(fiveToken.methods.balanceOf, setAccountBalance, accounts[0]);
+//     call(fiveToken.methods.hasStake, setAccountStakes, accounts[0]);
+//     call(fiveToken.methods.totalSupply, setTotalSupply);
+
+//     // await fiveToken.methods.totalSupply().call()
+//     // .then((result) => {
+//     //   setTotalSupply(result);
+//     // })
+//     // .catch((error) => {
+//     //   throw new Error(error);
+//     // })
+//     if (typeof totalSupply === 'number') {  
+//         console.log("totalSupply = ", totalSupply);
+//     }
+//   }
+
+//   // A hardcoded shortcut with FiveToken contract address from ganache 
+//   // GET CORRECT CONTRACT ADDRESS IN GANACHE
+//   function getFiveTokenContractAddress() {
+//     // return "0x1917b8513697Cf919eec8E848b139013c14C8402";
+//     return "0x30Dbdcb045f6EB1A058c10f2acdFC69C26e3F3c0";
+//   }
+
+//   function call(func, callback, ...args) {
+//     func(...args).call()
+//     .then((result) => {
+//       callback(result);
+//     })
+//     .catch((error) => {
+//       throw new Error(error);
+//     })
+//   }
+
+
+//   return (
+//     <div className="App">
+//         <header className="App-header">
+//             <h1> Welcome to your DAPP application</h1>
+//             <p> Account : {accounts[0]}</p>
+//             {totalSupply === 0 && <p>FiveToken total supply: 0</p>}
+//             {totalSupply !== 0 && <p>FiveToken total supply: {totalSupply}</p>}
+//             <p> Your FiveToken balance: {accountBalance}</p>
+//             <button ><p>Stake</p></button>
+//         </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- dappFive/project/migrations/1_initialMigrationFiveToken.js
+++ dappFive/project/migrations/1_initialMigrationFiveToken.js
@@ -3,7 +3,7 @@ const FiveToken = artifacts.require("FiveToken");
 // THis is an async function, it will accept the Deployer account, the network, and eventual accounts.
 module.exports = async function (deployer, network, accounts) {
   
-    await deployer.deploy(FiveToken, "FiveToken", "FIVE", 18, 5000000000); // 5 000 000 000
+    await deployer.deploy(FiveToken, "FiveToken", "FIVE", 18, 4000 /* 5000000000 */); // 5 000 000 000
   
     await FiveToken.deployed()
 
